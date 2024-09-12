cabdacb2c2b5aba5d7192e868dcd4abdd93e3666
chrslex <alexandro.tobing@gmail.com>
2024-09-12 15:59:58 +0700

    add controller placeholder file

---
+++ src/main/java/com/chrslex/demo/rest/ControllerRest.java
@@ -0,0 +1,18 @@
+package com.chrslex.demo.rest;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@RestController
+@RequestMapping("/test")
+/**
+ * RestController
+ */
+public class ControllerRest {
+
+    @GetMapping("/hello")
+    public String sayHello() {
+        return "Hello, World!";
+    }
+}
